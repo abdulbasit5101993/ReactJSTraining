98ae4b7f8fc53a109f31c77e695064dd84be6bbb
Neosoft <abdulbasit.peerzada@neosofttech.com>
2021-04-16 08:34:18 +0530

    changes in the code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Carousel from './Carousel.js';
 import Card from './Card.js';
 import Nav from './Nav.js';
 import Signup from './Signup.js';
+import Login from './Login.js';
 
+import {BrowserRouter as Router , Route, Redirect, Switch} from "react-router-dom";
 var myObj = {
         name: 'Choclate Truffle',
         image: 'carousel1.jpg',
@@ -13,8 +15,16 @@ var myObj = {
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Nav/>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/signup" exact component={Signup}></Route>
+        
+      </Router>
       <Nav/>
       <Signup/>
+      <Login/>
+      
       <Carousel/>
             <Card cakedata={myObj}/>
             <Card cakedata={myObj}/>
--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@
 import {Component} from "react"
 import axios from "axios"
 import { post } from "jquery"
-class Signup extends Component{
+class Login extends Component{
     constructor(){
         super()
         this.state={
@@ -19,13 +19,13 @@ class Signup extends Component{
         this.user.password = (event.target.value);
     }
 
-    register = ()=>{
+    login = ()=>{
         if( (this.user.email) && (this.user.password)){
             if( this.user.email === 'basit163@gmail.com' && this.user.password === '12345678'){
                 // this.setState({
                 //     message :  'logged in successfully'
                 // })
-                let apiUrl = "https://apibyashu.herokuapp.com/api/allcakes";
+                let apiUrl = "https://apibyashu.herokuapp.com/api/login";
                 axios({
                     url:apiUrl,
                     method: "get",
@@ -50,18 +50,17 @@ class Signup extends Component{
     }
     render(){
         return(
-
-            <div className="shadow col-4 offset-4 mt-4 mb-4 pt-4 pb-4">
-            <h2>Register</h2>
+        <div className="shadow col-4 offset-4 mt-4 mb-4 pt-4 pb-4">
+            <h2>Login Form</h2>
             <input className="form-control" placeholder="Enter Email" type="text" name="email" onChange={this.getEmail}></input>
             <input className="form-control mt-2" placeholder="Enter Password" type="password" name="password" onChange={this.getPassword}></input>
             <div className="text-danger mt-2">
             {this.state.message}
             </div>
-            <button className="btn btn-success mt-2" onClick={this.register}>Register</button>
+            <button className="btn btn-success mt-2" onClick={this.login}>Login</button>
         </div>
     );
     }
 }
 
-export default Signup
+export default Login
